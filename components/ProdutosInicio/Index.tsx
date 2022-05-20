b492f4b85c39d0fed63f66380f56455f5c5259a5
Rodrigo Henrique <rodhenrc@outlook.com>
2022-05-20 11:23:25 -0300

    Mudança na página inicial, em promoções

--- components/ProdutosInicio/Index.tsx
+++ components/ProdutosInicio/Index.tsx
@@ -3,11 +3,9 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import Item from "../Item/Item";
 import itemsList from "../../listaItems";
+import Promo from "./Promo";
 
 import styles from "../../styles/ProdutosInicio.module.scss";
 
@@ -27,24 +25,8 @@ interface Props {
 }
 
 function Index() {
-  const [left, setLeft] = useState(0);
   const [destaque, setDestaque] = useState<Props[]>([]);
   const [promo, setPromo] = useState<Props[]>([]);
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    function handleResize() {
-      setWidth(window.innerWidth);
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [width, setWidth]);
 
   useEffect(() => {
     getDestaque();
@@ -71,7 +53,7 @@ function Index() {
     let promos = itemsList.filter((item) => item.promo === true);
     let randPromo: Array<Props> = [];
 
-    while (randPromo.length < 6) {
+    while (randPromo.length < 8) {
       let newRand = Math.floor(Math.random() * promos.length);
       let newItem = promos[newRand];
 
@@ -81,46 +63,6 @@ function Index() {
     }
 
     setPromo([...randPromo]);
-  }
-
-  function getPosDireita() {
-    if (width < 650) {
-      return -1200;
-    } else if (width < 1300) {
-      return -900;
-    } else {
-      return -600;
-    }
-  }
-
-  function getPosDireitaExtra() {
-    if (width < 650) {
-      return -1250;
-    } else if (width < 1300) {
-      return -1100;
-    } else {
-      return -850;
-    }
-  }
-
-  let posDireita = getPosDireita();
-  let posDireitaExtra = getPosDireitaExtra();
-
-  function moverPromo(direcao: string) {
-    if (left === 0 && direcao === "direita") {
-      setLeft(0);
-    } else if (
-      (left === posDireita || left === posDireitaExtra) &&
-      direcao === "esquerda"
-    ) {
-      setLeft(posDireitaExtra);
-    } else if (direcao === "esquerda") {
-      setLeft((st) => st - 300);
-    } else if (left === posDireitaExtra && direcao === "direita") {
-      setLeft(posDireita);
-    } else if (direcao === "direita") {
-      setLeft((st) => st + 300);
-    }
   }
 
   return (
@@ -170,49 +112,7 @@ function Index() {
             </div>
           </Link>
         </div>
-        <div className={styles.containerPromocoes}>
-          <div className={styles.promocoesDesc}>
-            <div className={styles.desc}>
-              <h1>PROMOÇÕES</h1>
-              <span>São várias ofertas para você aproveitar</span>
-            </div>
-            <Link href={"/ofertas"} passHref>
-              <button>COMPRE JÁ</button>
-            </Link>
-          </div>
-          <div className={styles.promocoesItens}>
-            <FontAwesomeIcon
-              icon={faArrowLeft}
-              onClick={() => moverPromo("direita")}
-            />
-            <div className={styles.containerPromoItens}>
-              <div className={styles.itensPromo} style={{ left: `${left}px` }}>
-                {promo.map((i, key) => (
-                  <div className={styles.item} key={key}>
-                    <Item
-                      modelo={i.modelo}
-                      img={i.img}
-                      img2={i.img2}
-                      name={i.name}
-                      pathName={i.pathName}
-                      pPrazo={i.pPrazo}
-                      categoria={i.categoria}
-                      fabricante={i.fabricante}
-                      id={i.id}
-                      garantia={i.garantia}
-                      specs={i.specs}
-                      promo={i.promo}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-            <FontAwesomeIcon
-              icon={faArrowRight}
-              onClick={() => moverPromo("esquerda")}
-            />
-          </div>
-        </div>
+        <Promo promo={promo} />
       </div>
     </main>
   );
---
+++ components/ProdutosInicio/Promo.tsx
@@ -0,0 +1,143 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Item from "../Item/Item";
+
+import styles from "../../styles/ProdutosInicio.module.scss";
+
+interface Item {
+  modelo: string;
+  categoria: string;
+  img: string;
+  img2: string;
+  id: string;
+  fabricante: string;
+  name: string;
+  pathName: string;
+  pPrazo: number;
+  garantia: string;
+  specs: object[];
+  promo: boolean;
+}
+
+interface Props {
+  promo: Item[];
+}
+
+function Promo({ promo }: Props) {
+  const [index, setIndex] = useState({ left: 0, right: 0 });
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    getIndex();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [width]);
+
+  function getIndex() {
+    if (width < 768) {
+      setIndex({ left: 0, right: 0 });
+    } else if (width > 768 && width < 1299) {
+      setIndex({ left: 0, right: 1 });
+    } else if (width > 1300) {
+      setIndex({ left: 0, right: 2 });
+    }
+  }
+
+  function movePromo(direction: string) {
+    if (direction === "left" && index.left > 0) {
+      setIndex((prev) => ({
+        right: prev.right - 1,
+        left: prev.left - 1,
+      }));
+    }
+    if (direction === "right" && index.right < 7) {
+      setIndex((prev) => ({
+        right: prev.right + 1,
+        left: prev.left + 1,
+      }));
+    } else {
+      return;
+    }
+  }
+
+  return (
+    <div className={styles.containerPromocoes}>
+      <div className={styles.promocoesDesc}>
+        <div className={styles.desc}>
+          <h1>PROMOÇÕES</h1>
+          <span>São várias ofertas para você aproveitar</span>
+        </div>
+        <Link href={"/ofertas"} passHref>
+          <button>COMPRE JÁ</button>
+        </Link>
+      </div>
+      <div className={styles.promocoesItens}>
+        <div
+          className={
+            index.left !== 0
+              ? `${styles.promoLeft} ${styles.active}`
+              : styles.promoLeft
+          }
+        >
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            onClick={() => movePromo("left")}
+          />
+        </div>
+        <div className={styles.containerPromoItens}>
+          <div className={styles.itensPromo}>
+            {promo.map((i, key) => {
+              if (key >= index.left && key <= index.right) {
+                return (
+                  <div className={styles.item} key={key}>
+                    <Item
+                      modelo={i.modelo}
+                      img={i.img}
+                      img2={i.img2}
+                      name={i.name}
+                      pathName={i.pathName}
+                      pPrazo={i.pPrazo}
+                      categoria={i.categoria}
+                      fabricante={i.fabricante}
+                      id={i.id}
+                      garantia={i.garantia}
+                      specs={i.specs}
+                      promo={i.promo}
+                    />
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
+        <div
+          className={
+            index.right < 7
+              ? `${styles.promoRight} ${styles.active}`
+              : styles.promoRight
+          }
+        >
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            onClick={() => movePromo("right")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Promo;
